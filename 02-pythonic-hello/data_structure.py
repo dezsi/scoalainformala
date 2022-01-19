a1886830a26d44cea97afa552d4f7411193f7989
dezsi <dezsi.janos@yahoo.com>
2022-01-19 21:28:11 +0200

    curs02

---
+++ 02-pythonic-hello/data_structure.py
@@ -0,0 +1,31 @@
+#lista = [8, 2, -5, "string", 2.7]
+#print(type(lista))
+#print(lista[3])
+#print(lista[5])
+#print(lista[1:]) #afisam elementele de la indexul 1 inclusiv, pana la finalul listei
+#print(lista[1:3]) #
+#print(lista[1:6]) #
+#print(lista[0:5:2]) # elemenetele de la index 0 pana la indexul 5 (exclus) di 2 in 2
+# denumire_lista[index inceput:index_final:pas]
+#print(lista[-1]) # ultimul element din lista
+#print(lista[-1:-3:-1]) # ultimul element din lista
+#lista.append("mere")
+#lista[3] = "mere"
+#lista[5] = "mere"
+#print(len(lista))
+#print(lista.index("-5"))
+lista = [8, 2, -5, "string", 2.7]
+#lista.insert(3, "element nou")
+#lista.remove(-5)
+#lista.pop() # ultimul element din lista
+#lista.pop(2) # indexul element din lista
+#lista.clear()
+#lista.reverse()
+lista_stringuri = ["8", '2', '-5']
+print(ord('8'), '8')
+print(ord('2'), '2')
+print(ord('1'), '21')
+lista_stringuri.sort()
+#lista.pop(3)
+#lista.sort()
+print(lista_stringuri)
---
+++ 02-pythonic-hello/main.py
@@ -0,0 +1,65 @@
+#print("Numele meu este Alexandra")
+#print(1, type(-1)) # tipul int (intreg)
+#print(1.1, type(1.1)) #type float(rational/zecimal)
+#print(1j, type(1j)) # type complex
+#print(int(1.1))
+#print(float(1))
+#print(complex(1))
+#print(1+1)
+#print(1+1)
+#print(1*0)
+#print(1*3)
+#print(5 / 2)
+#print(5 // 2)
+#print(5 % 2)
+#print(5 ** 2)
+
+#print(5 == 2) #operatorul de egalitate +> returneaza un boolean ( True oFalse)
+#print (5 != 2) #operatorul de diferentiere +> returneaza un boolean ( True oFalse)
+#print(5 > 2)
+#print(5 < 2)
+#print(5 >= 2)
+#print(5 <= 2)
+
+#print(5 > 2 and 3 > 1) # operator logic "si"
+#print (5 > 2 or 3 > 1) # operator logic "sau"
+#print(not(5 > 2 and  3 > 1)) # operator de negare
+
+#print(7 is 7)
+#print (7 is not 7)
+
+
+
+#my_var = 0
+#my_var, my_var_1 = 0, 1
+#my_var = 0
+#my_var = my_var + 1 # asignare prin adunare / incrementare
+#my_var = 3
+#my_var *= 2 # asignare prin inmultire
+#print(my_var)
+
+
+#print("Ana are mere")
+#print('Ana are mere')
+
+#variabila = "Ana 'are' mere"
+#variabila = 'Ana "are" mere'
+#variabila = 'Ana \'are\' \n mere'  #randul urmator
+#variabila = """ Ana are
+#mere
+#"""
+#print(variabila)
+#nume = "Bratu"
+#prenume = "Alexandra"
+#varsta = 20
+#inaltime = 1.65
+
+#prezentare = "Numele meu este {0} si prenumele este {1}".format(nume, prenume)
+#prezentare = f"Numele meu este {nume} si prenumele este {prenume}."
+#prezentare = "Numele meu este  "+ nume + " si prenumele este " + prenume + " si varsta mea este" + str(varsta) + "."
+#prezentare = "Numele meu este {0} si prenumele {1} si varsta este {2}".format(nume, prenume, varsta)
+#prezentare = f"Numele meu este {nume} si prenumele {prenume} si varsta este {varsta}"
+#print(prezentare)
+#calcul = inaltime + varsta
+#calcul = nume + prenume
+#print(calcul)
